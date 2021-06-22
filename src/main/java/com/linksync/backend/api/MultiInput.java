4dab0c8d3102d120585f7a97db008e82f8447b46
Action <dev@action-server.com>
2021-06-23 00:43:10 +0200

    modifier public is redundant for interface methods

--- src/main/java/com/linksync/backend/api/MultiInput.java
+++ src/main/java/com/linksync/backend/api/MultiInput.java
@@ -3,7 +3,7 @@ package com.linksync.backend.api;
 import com.linksync.backend.nongate.Line;
 
 public interface MultiInput {
-  public Line getInput(int inputNum);
+  Line getInput(int inputNum);
 
-  public int getInputNum();
+  int getInputNum();
 }
