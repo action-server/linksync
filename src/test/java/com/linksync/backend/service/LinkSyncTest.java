d25581fbc857669259a498214d90a206a394aa3a
Action <dev@action-server.com>
2021-06-23 20:51:10 +0200

    Replaced manual generated components with resources

--- src/test/java/com/linksync/backend/service/LinkSyncTest.java
+++ src/test/java/com/linksync/backend/service/LinkSyncTest.java
@@ -1,18 +1,14 @@
 package com.linksync.backend.service;
 
-import com.linksync.backend.api.Connection;
-import com.linksync.backend.api.Link;
-import com.linksync.backend.gate.AndGate;
-import com.linksync.backend.gate.OrGate;
-import com.linksync.backend.gate.XorGate;
-import com.linksync.backend.nongate.*;
+import com.linksync.backend.nongate.Component;
+import com.linksync.backend.nongate.Display;
+import com.linksync.backend.nongate.OneBlock;
+import com.linksync.backend.nongate.ZeroBlock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,7 +21,7 @@ public class LinkSyncTest {
   }
 
   @Test
-  public void halfAdderTest(){
+  public void halfAdderTest() throws Exception {
     Component halfAdder = createHalfAdder();
     ZeroBlock zeroBlock = new ZeroBlock();
     OneBlock oneBlock = new OneBlock();
@@ -66,7 +62,7 @@ public class LinkSyncTest {
   }
 
   @Test
-  public void fullAdderTest(){
+  public void fullAdderTest() throws Exception {
     Component fullAdder = createFullAdder();
     ZeroBlock zeroBlock = new ZeroBlock();
     OneBlock oneBlock = new OneBlock();
@@ -110,49 +106,17 @@ public class LinkSyncTest {
     }
 }
 
-  private Component createHalfAdder(){
-    XorGate xorGate = new XorGate(2);
-    AndGate andGate = new AndGate(2);
-    List<Link> links = new ArrayList<>();
-    Map<String, List<Line>> inputs = new HashMap<>();
-    Map<String, Connection> outputs = new HashMap<>();
-    links.add(xorGate);
-    links.add(andGate);
-    inputs.put("A", List.of(xorGate.getInput(0), andGate.getInput(1)));
-    inputs.put("B", List.of(xorGate.getInput(1), andGate.getInput(0)));
-    outputs.put("Sum", xorGate);
-    outputs.put("Carry", andGate);
-    Component halfAdder = new Component(links, inputs, outputs, new ArrayList<>());
+  private Component createHalfAdder() throws Exception {
+    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("half_adder");
+    ComponentGenerator componentGenerator = new ComponentGenerator("");
+    Component halfAdder = componentGenerator.createComponentFromStram(inputStream);
     return halfAdder;
   }
 
-  private Component createFullAdder() {
-    Component halfAdder1 = createHalfAdder();
-    Component halfAdder2 = createHalfAdder();
-    halfAdder2.getInputs("A").stream().forEach(x -> halfAdder1.connect("Sum", x));
-    OrGate orGate = new OrGate(2);
-    halfAdder1.connect("Carry", orGate.getInput(0));
-    halfAdder2.connect("Carry", orGate.getInput(1));
-    List<Link> links = new ArrayList<>();
-    Map<String, List<Line>> inputs = new HashMap<>();
-    Map<String, Connection> outputs = new HashMap<>();
-    List<Component> components = new ArrayList<>();
-
-    links.addAll(halfAdder1.getLinks());
-    links.addAll(halfAdder2.getLinks());
-    links.add(orGate);
-
-    inputs.put("A", halfAdder1.getInputs("A"));
-    inputs.put("B", halfAdder1.getInputs("B"));
-    inputs.put("C", halfAdder2.getInputs("B"));
-
-    outputs.put("Sum", halfAdder2.getOutput("Sum"));
-    outputs.put("Carry", orGate);
-
-    components.add(halfAdder1);
-    components.add(halfAdder2);
-
-    Component fullAdder = new Component(links, inputs, outputs, components);
+  private Component createFullAdder() throws Exception {
+    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("full_adder");
+    ComponentGenerator componentGenerator = new ComponentGenerator("");
+    Component fullAdder = componentGenerator.createComponentFromStram(inputStream);
     return fullAdder;
   }
 }
