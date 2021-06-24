4eb1d8ca6cbb3375432d87139e4a25cca04b0a0e
Ahmed Elhori <dev@elhori.com>
2021-06-24 02:31:27 +0200

    Renamed abstract classes

--- src/main/java/com/linksync/backend/abstracts/AbstractMultiInputGate.java
+++ src/main/java/com/linksync/backend/abstracts/AbstractMultiInputGate.java
@@ -2,6 +2,7 @@ package com.linksync.backend.abstracts;
 
 import com.linksync.backend.api.MultiInput;
 import com.linksync.backend.nongate.Line;
+import com.linksync.backend.service.LinkSync;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,23 +15,33 @@ import java.util.function.BinaryOperator;
  */
 
 public abstract class AbstractMultiGate extends AbstractConnection implements MultiInput {
-  private final List<Line> inputs = new ArrayList<>();
+  private final List<Line> inputs;
+  private final List<Line> outputs;
   private final BinaryOperator<Boolean> function;
 
   /**
    * A constructor for AbstractMultiGate.
    *
    * @param inputNum is the number of inputs the logic gate is supposed to have.
+   * @param outputs is a list of output lines.
    * @param function is an implementation of the BinaryOperator interface.
    */
-  public AbstractMultiGate(int inputNum, BinaryOperator<Boolean> function) {
+  public AbstractMultiGate(int inputNum, List<Line> outputs, BinaryOperator<Boolean> function) {
+    super(outputs);
+    this.inputs=new ArrayList<>();
+    this.outputs=outputs;
     this.function = function;
     for (int i = 0; i < inputNum; i++) {
       inputs.add(new Line(i, this));
     }
+    LinkSync.followLink(this);
   }
 
   @Override
+  public void propagate() {
+    outputs.forEach(x -> x.setCurrent(result()));
+  }
+
   public boolean result() {
     boolean result = inputs.get(0).hasCurrent();
     for (int i = 1; i < inputs.size(); i++) {
--- src/main/java/com/linksync/backend/abstracts/AbstractUnaryInputGate.java
+++ src/main/java/com/linksync/backend/abstracts/AbstractUnaryInputGate.java
@@ -2,9 +2,10 @@ package com.linksync.backend.abstracts;
 
 import com.linksync.backend.api.UnaryInput;
 import com.linksync.backend.nongate.Line;
+import com.linksync.backend.service.LinkSync;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
+import java.util.List;
 import java.util.function.UnaryOperator;
 
 /**
@@ -12,13 +13,24 @@ import java.util.function.UnaryOperator;
  *
  * @author Ahmed Elhori
  */
-@RequiredArgsConstructor
 public abstract class AbstractUnaryGate extends AbstractConnection implements UnaryInput {
   @Getter
-  private final Line input = new Line(0, this);
+  private final Line input = new Line(this);
+  private final List<Line> outputs;
   private final UnaryOperator<Boolean> function;
 
+  public AbstractUnaryGate(List<Line> outputs, UnaryOperator<Boolean> function) {
+    super(outputs);
+    this.outputs=outputs;
+    this.function=function;
+    LinkSync.followLink(this);
+  }
+
   @Override
+  public void propagate() {
+    outputs.forEach(x -> x.setCurrent(result()));
+  }
+
   public boolean result() {
     return function.apply(input.hasCurrent());
   }
